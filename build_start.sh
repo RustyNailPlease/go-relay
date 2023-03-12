8efbf6f8cdecb499b1e802337fa85f8350a7ed25
RustyNailPlease <434500374@qq.com>
2023-03-12 08:48:16 +0800

    chore: shell

--- build_start.sh
+++ build_start.sh
@@ -1,6 +1,5 @@
-GO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags netgo  -o ./out/gorelay
-chmod +x out/gorelay
-sudo docker stop gorelay
-sudo docker rm gorelay
-sudo docker build --no-cache -t gorelay:v1.0 .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o out/gorelay
+docker stop gorelay
+docker rm gorelay
+docker build -t gorelay:v1.0 .
 sudo docker run -d --name gorelay --restart always --log-opt max-size=10m --log-opt max-file=3 -p 50000:50000  gorelay:v1.0
